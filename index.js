dfec5f28dc07160575a20d6d6d4a53f321929ff0
Will Lovell <willard.lovell@icloud.com>
2022-01-26 00:11:14 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const BASE_URL = "https://ghibliapi.herokuapp.com/"
+
+document.addEventListener('DOMcontentLoaded', () => {
+    // get movies and execute
+    getMovies()
+})
+
+
+
+
+function getMovies() {
+    const movieUL = document.getElementbyId('movie-list')
+}
