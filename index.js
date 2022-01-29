b0e63b4062dff4de28db4b35bc4de9249936413b
Will Lovell <willard.lovell@icloud.com>
2022-01-28 21:26:50 -0700

    more modal updates

--- index.js
+++ index.js
@@ -63,24 +63,14 @@ function favoriteMovieButton() {
 
 // MODAL FUNCTIONALITY
 const modal = document.getElementById("modal1");
-
-// Get the button that opens the modal
-const btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
+const btn = document.getElementById("modal-content");
+const span = document.getElementsByClassName("modal-close waves-effect waves-green btn-flat")[0];
 btn.onclick = function() {
     modal.style.display = "block";
 }
-
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
 }
-
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
