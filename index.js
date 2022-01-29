fcb8cd745c1b57d8a5499e700f8b9545557d9e2e
Will Lovell <willard.lovell@icloud.com>
2022-01-28 21:11:58 -0700

    testing for modal function

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 window.addEventListener('DOMContentLoaded', () => {
     // get movies and execute
     getMovies()
-
     favoriteMovieButton()
 
 
@@ -62,7 +61,28 @@ function favoriteMovieButton() {
     document.getElementById("demo").innerHTML = text;
 }
 
-document.addEventListener('click', function() {
-    const elems = document.getElementByClassName('modal');
-    const instances = M.Modal.init(elems, options);
-})
+// MODAL FUNCTIONALITY
+const modal = document.getElementById("modal1");
+
+// Get the button that opens the modal
+const btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
